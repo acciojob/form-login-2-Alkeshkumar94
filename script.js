c1e58b56f274ba7a6ef5a338249c3ae70ff2f6f3
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-08 21:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function myform(){
+	var fn=document.getElementById("firstname").value;
+	var ln=document.getElementById("lastname").value;
+	var cont=document.getElementById("contact").value;
+	var id=document.getElementById("id").value;
+
+	var result =
+		            "First Name: " + fn + "\n" +
+                   "Last Name: " + ln + "\n" +
+                   "Phone Number: " + cont + "\n" +
+                   "Email ID: " + id;
+
+	alert(result);
+}
+
